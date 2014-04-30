1d1c24cfe2af90da4a5031be54d9e2e56584dedb
Kasper Lewau <lewauu@gmail.com>
2014-04-30 09:14:33 +0200

    Remove bindElAttrOnce.

--- index.js
+++ index.js
@@ -40,26 +40,6 @@
   }]);
 
   /*
-    * Binds the attributes once to the element in question,
-    * Usage: <span bind-attr-once="{src: myExpression, title: myOtherExpression, alt: someOtherExpression}"></span>
-  */
-
-  module.directive('bindElAttrOnce', ['$parse', function ($parse) {
-    return {
-      compile: function (element, attributes) {
-        var expr = $parse(attributes.bindElAttrOnce);
-        return function link(scope, element, attrs) {
-          var values = expr(scope);
-
-          angular.forEach(values, function(value, key) {
-            element[0][key] = value;
-          });
-        };
-      }
-    };
-  }]);
-
-  /*
     * Watches the specified expression, and sends a notification to its child scopes
     * when it changes.  Used in conjunction with bind-on-notify directives, it allows
     * for bindings that update only when the watcher on this element fires.
